c031a8084716918b5ee68f38b2ea9d5d06591424
Csaba Pistyur <csaba.pistyur@gmail.com>
2021-11-03 15:20:27 +0100

    Implement: Junit test for getting the product once calling the getProductsForCategory method with a existing category id

--- src/test/java/com/codecool/shop/test/ServiceUnitTest.java
+++ src/test/java/com/codecool/shop/test/ServiceUnitTest.java
@@ -3,10 +3,16 @@ package com.codecool.shop.test;
 import com.codecool.shop.dao.ProductCategoryDao;
 import com.codecool.shop.dao.ProductDao;
 import com.codecool.shop.dao.SupplierDao;
+import com.codecool.shop.model.Product;
 import com.codecool.shop.model.ProductCategory;
 import com.codecool.shop.model.Supplier;
 import com.codecool.shop.service.ProductService;
 import org.junit.jupiter.api.Test;
+
+import java.math.BigDecimal;
+import java.util.ArrayList;
+import java.util.List;
+
 import static org.mockito.Mockito.*;
 import static org.junit.jupiter.api.Assertions.*;
 public class ServiceUnitTest {
@@ -64,7 +70,27 @@ public class ServiceUnitTest {
         when(supplierDao.find(nonExistingSupplierId)).thenReturn(null);
 
         assertNull(productService.getProductSupplier(nonExistingSupplierId));
+    }
 
+    @Test
+    void getProductsForCategory_existing_cat_id_return_products() {
+        ProductDao productDao = mock(ProductDao.class);
+        ProductCategoryDao productCategoryDao = mock(ProductCategoryDao.class);
+        SupplierDao supplierDao = mock(SupplierDao.class);
+        ProductService productService = new ProductService(productDao, productCategoryDao, supplierDao);
+
+        ProductCategory productCategory = new ProductCategory("testName", "testDepartment", "testDescription");
+        Supplier supplier = new Supplier("testSupplierName", "testSupplierDescription");
+        Product testProduct = new Product("testProductName", new BigDecimal(999), "USD", "testDescription", productCategory, supplier);
+
+        List<Product> testProductList = new ArrayList<>();
+        testProductList.add(testProduct);
+
+        int existingCatId = 999;
+        when(productCategoryDao.find(existingCatId)).thenReturn(productCategory);
+        when(productDao.getBy(productCategory)).thenReturn(testProductList);
+
+        assertEquals(testProductList, productService.getProductsForCategory(999));
     }
 
 
